8ca798da27b45910351c0c26ddea2609e5c1cd2a
Dan Waltin <dan@waltin.se>
2021-02-13 08:17:38 +0100

    Adding a collapsible sidebar

--- SwiftUI-Layout/ContentView.swift
+++ SwiftUI-Layout/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+struct Character : Identifiable {
+	let id = UUID()
+	let name: String
+}
+
+let litterature = [
+	Character(name: "Ronja Rövardotter"),
+	Character(name: "Pippi Långstrump")
+]
+
+let science = [
+	Character(name: "Ada Lovelace"),
+	Character(name: "Alan Turing"),
+	Character(name: "Albert Einstein"),
+	Character(name: "Marie Curie")
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			List {
+				Section(
+					header: Text("Litterature"),
+					content: {
+						ForEach(litterature) {character in
+							NavigationLink(
+								destination: Text(character.name),
+								label: {
+									Text(character.name)
+										.fixedSize()
+								}
+							)
+						}
+					})
+				Section(
+					header: Text("Science"),
+					content: {
+						ForEach(science) {character in
+							NavigationLink(
+								destination: Text(character.name),
+								label: {
+									Text(character.name)
+										.fixedSize()
+								}
+							)
+						}
+					})
+			}
+			.listStyle(SidebarListStyle())
+
+		}
+		.toolbar(content: {
+			ToolbarItemGroup(placement: .navigation) {
+				Button(action: {NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)}) {
+					Image(systemName: "sidebar.left")
+				}
+				.help("Toggle Sidebar")
+			}
+
+		})
     }
 }
 
