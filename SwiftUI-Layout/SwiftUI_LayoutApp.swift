c75d22ff706d78a0c4f66924d035709205e38f30
Dan Waltin <dan@waltin.se>
2021-02-13 08:02:22 +0100

    added xcode project

---
+++ SwiftUI-Layout/SwiftUI_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LayoutApp.swift
+//  SwiftUI-Layout
+//
+//  Created by Dan Waltin on 2021-02-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
