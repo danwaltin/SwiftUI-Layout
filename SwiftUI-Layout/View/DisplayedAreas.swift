b6354592ee3c46cf30c753f999c1a5dd58024540
Dan Waltin <dan@waltin.se>
2021-02-15 16:11:52 +0100

    Adding class DisplayedAreas

---
+++ SwiftUI-Layout/View/DisplayedAreas.swift
@@ -0,0 +1,12 @@
+//
+//  DisplayedAreas.swift
+//  SwiftUI-Layout
+//
+//  Created by Dan Waltin on 2021-02-14.
+//
+
+import Foundation
+
+class DisplayedAreas: ObservableObject {
+	@Published var displayToolsArea = true
+}
--- SwiftUI-Layout/View/Toolbar.swift
+++ SwiftUI-Layout/View/Toolbar.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 
 struct SidebarToolbar: ToolbarContent {
-    var body: some ToolbarContent {
+	var body: some ToolbarContent {
 		ToolbarItemGroup(placement: .primaryAction) {
 			Button(action: {NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)}) {
 				Image(systemName: "sidebar.left")
 			}
 			.help("Hide or show the sidebar")
 		}
-    }
+	}
 }
 
 struct CharacterToolbar: ToolbarContent {
+	@Binding var displayedAreas: DisplayedAreas
+	
 	var body: some ToolbarContent {
 		ToolbarItem {
 			Button(action: {
-				print("Item button")
+				print("*** Clicked toggle tools button. displayedAreas.displayToolsArea: \(displayedAreas.displayToolsArea)")
 			}){
-			 Image(systemName: "sidebar.right")
+				Image(systemName: "sidebar.right")
 			}
 			.help("Hide or show the tools")
 		}
